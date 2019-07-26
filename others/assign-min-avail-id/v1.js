81f34c024f9edb254fe48b5b2b1b8767996478a6
scottliu2011 <scottliu2011@gmail.com>
2019-07-26 13:57:15 +0800

    代码优化改进

--- others/assign-min-avail-id/v1.js
+++ others/assign-min-avail-id/v1.js
@@ -1,12 +1,16 @@
+// 暴力搜索
 
 function assign(array) {
-  for (let i = 0; i < array.length; i++) {
+  const count = array.length;
+
+  for (let i = 0; i < count; i++) {
     if (!array.includes(i)) {
       return i;
     }
   }
 
-  return array.length;
+  // 如果在数组中没找到 count就是最小可分配ID
+  return count;
 }
 
 const array = [5, 3, 0, 4, 1, 9, 7, 2, 6, 8];
